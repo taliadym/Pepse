f3dac6b76c32e56414a3a3779274cce9ff7ae045
Talia Rigler <taliarigler@h-MacBook-Air-sl-Talia.local>
2022-12-29 11:36:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+import danogl.GameManager;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        new GameManager().run();
+    }
+}
